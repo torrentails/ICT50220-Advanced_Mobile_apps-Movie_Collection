35b649012733d121755b22b64d51c07f37292e9c
Alex Sullivan <torrentails@torrentails.com>
2023-08-14 18:13:37 +0800

    Implements AverageRating

--- MovieCollection-AlexS/MovieCollection-AlexS/MovieOwner.cs
+++ MovieCollection-AlexS/MovieCollection-AlexS/MovieOwner.cs
@@ -20,5 +20,22 @@ namespace MovieCollection_AlexS
             this.email = email;
             this.movies = new List<Movie>();
         }
+
+        public float AverageRating(bool ignoreUnrated = true)
+        {
+            float rating = 0f;
+            int numMovies = 0;
+
+            foreach (Movie movie in movies)
+            {
+                if (!ignoreUnrated || movie.hasBeenRated)
+                {
+                    rating += movie.getRating();
+                    numMovies++;
+                }
+            }
+
+            return rating / numMovies;
+        }
     }
 }
