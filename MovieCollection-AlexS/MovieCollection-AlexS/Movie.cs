d5905fe89ae9d8303ab7276791d7eead2c549ae4
Alex Sullivan <torrentails@torrentails.com>
2023-08-14 16:08:09 +0800

    Adds MovieOwner and Movie

---
+++ MovieCollection-AlexS/MovieCollection-AlexS/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieCollection_AlexS
+{
+    internal class Movie
+    {
+    public String name;
+    public int duration;
+    public int year;
+    public String star;
+    public enum contentRating
+    {
+        G = 0,
+        PG = 1,
+        M = 2,
+        MA = 3,
+        R = 4,
+        X = 5,
+        CTC = 6
+    }
+    }
+}
---
+++ MovieCollection-AlexS/MovieCollection-AlexS/MovieOwner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieCollection_AlexS
+{
+
+    internal class MovieOwner
+    {
+        public String name;
+        public int age;
+        public String email;
+
+        public MovieOwner(String name, int age, String email)
+        {
+            this.name = name;
+            this.age = age;
+            this.email = email;
+        }
+    }
+}
